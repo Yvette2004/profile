fb1cd45f7dcabea97e7942e39e03cc79641834af
yvette rodriguez <yvette.rodriguez@csedge.org>
2019-02-09 12:28:45 -0800

    ugh

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
-let newHeading = document.createElement("h1");
-newHeading.innerHTML = "Hello!!!";
+
 let about = document.getElementById("about");
-about.appendChild(newHeading);
+let bodyArray= document.getElementsByTagName("BODY");
+let body = bodyArray[0];
+document.addEventListener("click",whenClicked);
+
+about.addEventListener("mouseover",whenHovered);
+about.addEventListener("mouseout",whenDoneHovering);
+function whenClicked() {
+	let newHeading = document.createElement("h1");
+	newHeading.innerHTML = "Hello!!!";
+	newHeading.style.color = "blue";
+	newHeading.style.fontSize = "28px";
+	about.appendChild(newHeading);
+}
+function whenHovered(){
+	body.style.color = "pink";
+}
+function whenDoneHovering(){
+	body.style.color = "white";
+}
+
